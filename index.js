8c4976c679a44fe0fd15e3eb3f5926095ba86441
rakesh <reakes@gmail.com>
2024-02-24 17:54:33 +0530

    updated index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const {chats} =require('./data')
 
-const port=process.env.PORT || 5000
+const port=process.env.PORT || 3000
 app.get('/',(req,res)=>{
     res.send("All work is working well")
 })
