3dc44a1a341a42b4ce43c679d25b233ca2615979
rakesh <reakes@gmail.com>
2024-02-24 17:47:39 +0530

    updated index.js

---
+++ data.js
@@ -0,0 +1,13 @@
+const chats=[
+
+
+    {
+        id:1,
+        name:"rakesh"
+    },
+    {
+        id:2,
+        name:"prakes"
+    }
+]
+module.exports={chats}
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express=require('express')
+const app=express()
+const {chats} =require('./data')
+
+const port=process.env.PORT || 5000
+app.get('/',(req,res)=>{
+    res.send("All work is working well")
 })
-app.listen(process.env.PORT || 3000)
+app.get('/api/chats',(req,res)=>{
+   res.send(chats)
+})
+
+
+app.get('/api/chats/:id',(req,res)=>{
+
+    const {id}=req.params
+   const result=chats.find((element,index)=>{
+    return element._id===id
+   });
+if(result){
+    res.send(result.chatName)
+
+}
+else{
+    res.send('id not found')
+}
+
+})
+
+app.get('/*',(req,res)=>{
+    res.send("error 404 page not found")
+})
+app.listen(port,()=>{
+    console.log("server is listening")
+})
